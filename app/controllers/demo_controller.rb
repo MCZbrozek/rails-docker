11a3f0e311d0f9c22f904307177eb99f49c7ac2d
Mike Zbrozek <mzbrozek@cnm.edu>
2023-01-17 10:38:53 -0700

    Passing Params to URL

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,5 +3,26 @@ class DemoController < ApplicationController
   layout false
 
   def index
+    # the default is render('index')
+    # render is oftern used with if/else statements
+    render ('index')
+    
   end
+
+  def hello
+    @array = [1,2,3,4,5]
+    @id = params['id']
+    @page = params[:page]
+    render ('hello')
+  end
+
+  def other_hello
+    redirect_to(:controller => 'demo', :action => 'index')
+    # The routed to the action 'index' which renders the 'index' action. This is often used for sign in verificication etc, or when the browser needs to perfom another task before rendering a selected page. 
+  end
+
+  def mike_resume
+    redirect_to('http://resume.mikezbrozek.codes', allow_other_host:true)
+    
+  end   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'demo#index'
+
   get 'demo/index'
+  get 'demo/hello'
+  get 'demo/other_hello'
+  get 'demo/mike_resume'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
